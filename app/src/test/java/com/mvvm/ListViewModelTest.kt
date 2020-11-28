88d8a8c7cf0d3a196474632562cd90454ab2047c
aditya <mishraaditya595@gmail.com>
2020-11-28 16:54:37 +0530

    started with unit testing

---
+++ app/src/test/java/com/mvvm/ListViewModelTest.kt
@@ -0,0 +1,31 @@
+package com.mvvm
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.reactivex.Scheduler
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Before
+import org.junit.Rule
+import java.util.concurrent.Executor
+
+class ListViewModelTest
+{
+    @get:Rule
+    var rule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupRxSchedulers()
+    {
+        val immediate = object: Scheduler(){
+            override fun createWorker(): Worker
+            {
+                return ExecutorScheduler.ExecutorWorker(Executor {
+                    it.run()
+                },true)
+            }
+        }
+        RxJavaPlugins.setInitNewThreadSchedulerHandler { scheduler -> immediate }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler ->  immediate}
+    }
+}
